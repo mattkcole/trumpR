d2d37179d21846d2979aa5fbfb0aabeffbe9f427
matt cole <mattcole1093@gmail.com>
2016-11-11 00:22:00 -0500

    ui edited: added all params

--- trumprapp/ui.R
+++ trumprapp/ui.R
@@ -13,16 +13,51 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
+  titlePanel("TrumpR"),
+  # apprehension bed_day per_day avg_day legal hearing transport
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
+        numericInput("apprehension.user",
+                   "Cost to apprehend",
+                   min = 500,
+                   max = 20000,
+                   value = 18310),
+       sliderInput("avg_day.user",
+                   "Days in detention",
                    min = 1,
-                   max = 50,
-                   value = 30)
+                   max = 100,
+                   value = 30),
+       numericInput("per_day.user",
+                   "Cost per day of detention (staffing)",
+                   min = 1,
+                   max = 100,
+                   value = 30),
+       numericInput("bed_day.user",
+                    "Cost per day of detention (housing)",
+                    min = 1,
+                    max = 100,
+                    value = 30),
+       numericInput("per_day.user",
+                    "Cost per day of detention (staffing)",
+                    min = 1,
+                    max = 100,
+                    value = 30),
+       numericInput("legal.user",
+                    "Cost per day of detention (staffing)",
+                    min = 1,
+                    max = 100,
+                    value = 30),
+       numericInput("hearing.user",
+                    "Cost per day of detention (staffing)",
+                    min = 1,
+                    max = 100,
+                    value = 30),
+       numericInput("transport.user",
+                    "Cost per day of detention (staffing)",
+                    min = 1,
+                    max = 100,
+                    value = 30)
     ),
     
     # Show a plot of the generated distribution
